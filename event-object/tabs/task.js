1dd12e32c710998652188726c2fb63e2a01da592
Marina <marina.nogovitsyna.98@mail.ru>
2023-11-05 11:23:29 +0300

    Еще вариант решения задания 3

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,21 +1,32 @@
 const tabArr = Array.from(document.querySelectorAll(".tab"));
 const contentArr = Array.from(document.querySelectorAll(".tab__content"));
-console.log(tabArr)
 
-for (let i = 0; i < tabArr.length; i++){
-    tabArr[i].addEventListener('click', () => {
-        tabArr[i].classList.add('tab_active')
-        contentArr[i].classList.add('tab__content_active')
-        for(let j = 0; j < tabArr.length; j++) {
-            if (j !== i) {
-                tabArr[j].classList.remove('tab_active')
-                contentArr[j].classList.remove('tab__content_active')
-            }
-        }
-    })
+// for (let i = 0; i < tabArr.length; i++){
+//     tabArr[i].addEventListener('click', () => {
+//         tabArr[i].classList.add('tab_active')
+//         contentArr[i].classList.add('tab__content_active')
+//         for(let j = 0; j < tabArr.length; j++) {
+//             if (j !== i) {
+//                 tabArr[j].classList.remove('tab_active')
+//                 contentArr[j].classList.remove('tab__content_active')
+//             }
+//         }
+//     })
+// }
+
+// Вариант с indexOf
+
+tabArr.forEach((el) =>
+  el.addEventListener("click", () => changeClasses(tabArr.indexOf(el)))
+);
+
+function changeClasses(i) {
+  tabArr[i].classList.add("tab_active");
+  contentArr[i].classList.add("tab__content_active");
+  for (let j = 0; j < tabArr.length; j++) {
+    if (j !== i) {
+      tabArr[j].classList.remove("tab_active");
+      contentArr[j].classList.remove("tab__content_active");
+    }
+  }
 }
-
-
-
-
-
